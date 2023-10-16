0c6e4a9c877fa7185627c894eaa91f1d3666b247
Manasa Maram <devendra.v@prodapt.com>
2023-10-16 14:46:40 +0530

    Change schema for id

--- app/app/schemas/channel_ref_schema.py
+++ app/app/schemas/channel_ref_schema.py
@@ -4,8 +4,16 @@ from pydantic import BaseModel, ConfigDict, Field
 class ChannelRef(BaseModel):
     model_config = ConfigDict(from_attributes=True, populate_by_name=True)
 
+    id: str | None = Field(
+        None,
+        description=(
+            "Identifier of the note within its containing entity "
+            "(may or may not be globally unique, depending on provider implementation)"
+        ),
+    )
+
     href: str | None = Field(None, description=" Reference of the channel.")
-    id: str | None = Field(None, description="Identifier of the channel.")
+    # id: str | None = Field(None, description="Identifier of the channel.")
     name: str | None = Field(None, description="Name of the channel.")
     referred_type: str | None = Field(
         None,
--- app/app/schemas/resource_ref_schema.py
+++ app/app/schemas/resource_ref_schema.py
@@ -6,7 +6,15 @@ from pydantic import BaseModel, ConfigDict, Field
 class ResourceRef(BaseModel):
     model_config = ConfigDict(from_attributes=True, populate_by_name=True)
 
-    object_id: str | None = Field(None, alias="id", description="Identifier of the Resource")
-    name: str | None = Field(None, description="Name of the resource collection")
+    id: str | None = Field(
+        None,
+        description=(
+            "Identifier of the note within its containing entity "
+            "(may or may not be globally unique, depending on provider implementation)"
+        ),
+    )
+
+    object_id: str | None = Field(None, description="Identifier of the Resource")
+    name: str | None = Field(..., description="Name of the resource collection")
     href: str | None = Field(None, description="Hyperlink to access the Resource")
     description: str | None = Field(None, description="Description of the Resource")
