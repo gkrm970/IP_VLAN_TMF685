a15294b5044cd1340008f5b95a3d37ca4c8924d5
Manasa Maram <manasa.m@prodapt.com>
2023-11-17 18:49:57 +0530

    Update remaining capacity amount in resource pool patch Api.

---
+++ app/app/models/resource_pool_resource_model.py
@@ -0,0 +1,37 @@
+import uuid
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, ForeignKey, Integer
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship,
+)
+from app import schemas
+from app.db.base import BaseDbModel
+
+# Cascade rule for SQLAlchemy relationship. The all symbol is a synonym for save-update,
+# merge, refresh-expire, expunge, delete, and using it in conjunction with delete-orphan
+# indicates that the child object should follow along with its parent in all cases, and
+# be deleted once it is no longer associated with that parent
+_ALL_DELETE_ORPHAN = "all, delete-orphan"
+if TYPE_CHECKING:
+    from app.models import ResourcePoolCapacity
+
+
+class ResourcePoolResource(BaseDbModel):
+    id: Mapped[str] = mapped_column(String(255), primary_key=True, index=True)
+
+    resource_id: Mapped[str] = mapped_column(String(255))
+    href: Mapped[str] = mapped_column(String(255))
+
+    resource_pool_capacity_id: Mapped[str] = mapped_column(ForeignKey("resource_pool_capacity.id"))
+    resource_pool_capacity: Mapped["ResourcePoolCapacity"] = relationship(back_populates="resource_pool_resource")
+
+    @classmethod
+    def from_schema(cls, schema: schemas.ResourcePoolResource) -> "ResourcePoolResource":
+        return cls(
+            id=str(uuid.uuid4()),
+            resource_id=schema.resource_id,
+            href=schema.href
+        )
---
+++ app/app/schemas/resource_pool_resource_schema.py
@@ -0,0 +1,18 @@
+import datetime
+
+from pydantic import BaseModel, ConfigDict, Field
+
+from app import schemas
+
+_NAME_DESCRIPTION = "A string used to give a name to the resource"
+
+
+class ResourcePoolResource(BaseModel):
+    model_config = ConfigDict(from_attributes=True, populate_by_name=True)
+
+    resource_id: str | None = Field(
+        None,
+        description=_NAME_DESCRIPTION)
+    href: str | None = Field(
+        None,
+        description=_NAME_DESCRIPTION)
