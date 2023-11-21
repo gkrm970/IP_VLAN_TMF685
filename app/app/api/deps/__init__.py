468e260a8d70c4413f638a3e54e4531f0b12d14b
Nishitha D <nishitha.d@prodapt.com>
2023-11-21 17:10:30 +0530

    Fix typos and formatting
    Signed-off-by: Nishitha D <nishitha.d@prodapt.com>

--- app/app/api/deps/__init__.py
+++ app/app/api/deps/__init__.py
@@ -1,4 +1,7 @@
-from app.core.security import validate_token_signature  # noqa: F401
+from app.core.security import (  # noqa: F401
+    validate_token_signature,
+    AccessRoleValidator,
+)
 from .db_deps import get_db_session
 from .query_param_deps import FieldsQuery, LimitQuery, OffsetQuery
 from .resource_deps import get_reservation  # noqa: F401
