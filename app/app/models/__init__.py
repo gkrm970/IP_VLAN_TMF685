9ddd7a1233aea28fa8389f4008c8bb2743a086e6
Nishitha D <nishitha.d@prodapt.com>
2023-11-22 14:30:15 +0000

    Update models __init__.py to avoid circular imports

--- app/app/models/__init__.py
+++ app/app/models/__init__.py
@@ -1,9 +1,8 @@
 from .resource_pool_applicable_time_period_model import ResourcePoolApplicableTimePeriod
-from .resource_pool_resource_specification_model import (
-    ResourcePoolResourceSpecification,
-)
+from .resource_pool_resource_specification_model import ResourcePoolResourceSpecification
 from .resource_pool_place_model import ResourcePoolPlace
 from .resource_pool_related_party_model import ResourcePoolRelatedParty
+from .resource_pool_resource_model import ResourcePoolResource
 from .resource_pool_capacity_model import ResourcePoolCapacity
 from .resource_pool_model import ResourcePool
 from .reservation_applicable_time_period_model import (
@@ -11,15 +10,19 @@ from .reservation_applicable_time_period_model import (
 )  # noqa F401
 from .reservation_place_model import ReservationPlace  # noqa F401
 from .reservation_resource_pool_model import ReservationResourcePool  # noqa F401
-from .reservation_external_party_characteristics_model import (
-    ExternalPartyCharacteristics,
-)
+from .reservation_external_party_characteristics_model import ExternalPartyCharacteristics
+from .reservation_characteristic_model import Characteristic
+from .reservation_resource_model import ReservationResource
+from .reservation_applied_capacity_amount_model import AppliedCapacityAmount
 from .reservation_resource_capacity_model import (
     ReservationResourceCapacity,
 )  # noqa F401
+from .reservation_resource_name_model import ReservationResourceName
 from .reservation_item_model import ReservationItem  # noqa F401
 from .reservation_related_party_model import ReservationRelatedParty
 from .reservation_requested_period_model import ReservationRequestedPeriod
 from .reservation_valid_for_model import ValidFor
 
 from .reservation_model import Reservation  # isort: split
+
+# from .resource_specification_model import ResourceSpecification  # isort: split
