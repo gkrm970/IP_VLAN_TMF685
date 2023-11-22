6949cb6af5392c456db19dd70b51c74b87291db4
Nishitha D <nishitha.d@prodapt.com>
2023-11-22 14:33:47 +0000

    Update schema __init__.py to avoid circular import

--- app/app/schemas/__init__.py
+++ app/app/schemas/__init__.py
@@ -1,31 +1,46 @@
-from .reservation_applicable_time_period_schema import (  # noqa F401
+from .resource_pool_resource_specification_schema import ResourcePoolResourceSpecification
+from .resource_pool_related_party_schema import ResourcePoolRelatedParty
+from .resource_pool_place_schema import ResourcePoolPlace
+from .resource_pool_applicable_time_period_schema import ResourcePoolApplicableTimePeriod
+from .reservation_external_party_characteristics_schema import ExternalPartyCharacteristics
+from .resource_pool_resource_schema import ResourcePoolResource
+from .resource_pool_capacity_schema import ResourcePoolCapacity
+from .resource_pool_capacity_schema import ResourcePoolCapacityCreate,ResourcePoolCapacity, ResourcePoolCapacityUpdate
+from .resource_pool_schema import ResourcePool, ResourcePoolCreate, ResourcePoolUpdate
+from .reservation_applicable_time_period_schema import (
     ReservationApplicableTimePeriod,
-)
-from .reservation_external_party_characteristics_schema import (
-    ExternalPartyCharacteristics,
-)
-from .reservation_item_schema import ReservationItem  # noqa F401
+)  # noqa F401
 from .reservation_place_schema import Place  # noqa F401
+from .reservation_resource_pool_schema import ReservationResourcePool  # noqa F401
+from .reservation_resource_capacity_schema import (
+    ReservationResourceCapacity,
+)  # noqa F401
+from .reservation_characteristic_schema import ReservationCharacteristic
+from .reservation_resource_schema import ReservationResource
+from .reservation_applied_capacity_amount_schema import AppliedCapacityAmount
+from .reservation_resource_name_schema import ReservationResourceName
+from .reservation_item_schema import ReservationItem  # noqa F401
+from .reservation_item_schema import ReservationItemCreate, ReservationItemUpdate
+
+# from .applicable_time_period_schema import ApplicableTimePeriod  # noqa F401
+# from .resource_ref_schema import ResourceRef
+# from .resource_pool_schema import ResourcePool
+# from .place_in_resource_capacity_demand_schema import PlaceResourceCapacityDemand
+
+# from .resource_capacity_demand_schema import ResourceCapacityDemand
+
+# from .requested_period_schema import RequestedPeriod
+# from .channel_ref_schema import ChannelRef
+# from .product_offering_ref_schema import ProductOfferingRef
+
 from .reservation_related_party_schema import RelatedParty
 from .reservation_requested_period_schema import ReservationRequestedPeriod
-from .reservation_resource_capacity_schema import (  # noqa F401
-    ReservationResourceCapacity,
-)
-from .reservation_resource_pool_schema import ReservationResourcePool  # noqa F401
-from .reservation_schemas import (  # isort: split
-    Reservation,
+from .reservation_valid_for_schema import ReservationValidFor
+
+from .reservation_schemas import (
     ReservationBase,
+    Reservation,
     ReservationCreate,
     ReservationUpdate,
-)
-from .resource_pool_applicable_time_period_schema import (
-    ResourcePoolApplicableTimePeriod,
-)
-from .resource_pool_capacity_schema import ResourcePoolCapacity
-from .resource_pool_place_schema import ResourcePoolPlace
-from .resource_pool_related_party_schema import ResourcePoolRelatedParty
-from .resource_pool_resource_specification_schema import (
-    ResourcePoolResourceSpecification,
-)
-from .resource_pool_schema import ResourcePool, ResourcePoolCreate, ResourcePoolUpdate
+)  # isort: split
 from .token_schema import TokenAccessDetails, TokenPayload  # noqa F401
