2c193bdb8854fef92884b0a2d40a509ab372a0c8
Gopal CH <gopalakrishna.c@prodapt.com>
2023-11-25 13:51:49 +0530

    modify init files for schemas and models

--- app/app/schemas/__init__.py
+++ app/app/schemas/__init__.py
@@ -1,21 +1,11 @@
-from .resource_pool_resource_specification_schema import (
-    ResourcePoolResourceSpecification,
-)
+from .resource_pool_resource_specification_schema import ResourcePoolResourceSpecification
 from .resource_pool_related_party_schema import ResourcePoolRelatedParty
 from .resource_pool_place_schema import ResourcePoolPlace
-from .resource_pool_applicable_time_period_schema import (
-    ResourcePoolApplicableTimePeriod,
-)
-from .reservation_external_party_characteristics_schema import (
-    ExternalPartyCharacteristics,
-)
+from .resource_pool_applicable_time_period_schema import ResourcePoolApplicableTimePeriod
+from .reservation_external_party_characteristics_schema import ExternalPartyCharacteristics
 from .resource_pool_resource_schema import ResourcePoolResource
 from .resource_pool_capacity_schema import ResourcePoolCapacity
-from .resource_pool_capacity_schema import (
-    ResourcePoolCapacityCreate,
-    ResourcePoolCapacity,
-    ResourcePoolCapacityUpdate,
-)
+from .resource_pool_capacity_schema import ResourcePoolCapacityCreate,ResourcePoolCapacity, ResourcePoolCapacityUpdate
 from .resource_pool_schema import ResourcePool, ResourcePoolCreate, ResourcePoolUpdate
 from .reservation_applicable_time_period_schema import (
     ReservationApplicableTimePeriod,
@@ -31,7 +21,6 @@ from .reservation_applied_capacity_amount_schema import AppliedCapacityAmount
 from .reservation_resource_name_schema import ReservationResourceName
 from .reservation_item_schema import ReservationItem  # noqa F401
 from .reservation_item_schema import ReservationItemCreate, ReservationItemUpdate
-
 from .reservation_related_party_schema import RelatedParty
 from .reservation_requested_period_schema import ReservationRequestedPeriod
 from .reservation_valid_for_schema import ReservationValidFor
